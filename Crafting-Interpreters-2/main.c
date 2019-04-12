31c4ec65d5e6ffd9dcd09abe9b0c487c89122d00
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2019-04-12 14:05:22 +0200

    Initial Commit

---
+++ Crafting-Interpreters-2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Crafting-Interpreters-2
+//
+//  Created by Michal Matuszczyk on 12/04/2019.
+//  Copyright © 2019 Michal Matuszczyk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
