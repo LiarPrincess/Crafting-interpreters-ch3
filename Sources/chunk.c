35a63942572e659de1596d3fa2d520b1371e45d0
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2019-04-17 22:23:09 +0200

    4.2

--- Sources/chunk.c
+++ Sources/chunk.c
@@ -35,7 +35,3 @@ int addConstant(Chunk* chunk, Value value) {
   writeValueArray(&chunk->constants, value);
   return chunk->constants.count  - 1;
 }
-
-void printValue(Value value) {
-  printf("%g", AS_NUMBER(value));
-}
