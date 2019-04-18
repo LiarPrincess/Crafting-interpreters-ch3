a0c1f2ee2886e1ba646fb5d6dc6091d27146ec2e
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2019-04-18 20:17:20 +0200

    5.2

--- Sources/object.h
+++ Sources/object.h
@@ -29,6 +29,10 @@ static inline bool isObjType(Value value, ObjType type) {
   return IS_OBJ(value) && AS_OBJ(value)->type == type;
 }
 
+// Take ownership of existing string
+ObjString* takeString(char* chars, int length);
+
+// Create new string based on arg and take ownership of it
 ObjString* copyString(const char* chars, int length);
 
 void printObject(Value value);
